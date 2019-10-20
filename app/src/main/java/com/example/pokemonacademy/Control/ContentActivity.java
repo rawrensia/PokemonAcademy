e6650acfcedd86fbd9546e6eb96a08c17d0a63ad
rawrensia <laurenerual@hotmail.com>
2019-10-20 23:56:46 +0800

    Updated learning material text styles

--- app/src/main/java/com/example/pokemonacademy/Control/ContentActivity.java
+++ app/src/main/java/com/example/pokemonacademy/Control/ContentActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.text.Html;
 
 import com.example.pokemonacademy.R;
 
@@ -27,6 +28,34 @@ public class ContentActivity extends AppCompatActivity {
 
         TextView tv = findViewById(R.id.content_textView);
         tv.setText(worldName);
+
+        String planning = "<h5> <font color=\"#784C27\">Requirements:</h5>"
+                + "<p>They are specifications of what should be implemented, " +
+                "descriptions of how a system should behave, or of a system property/attribute.</p>"
+                + "<h5> <font color=\"#784C27\"> Requirement Engineering</h5><p>It is about identifying " +
+                "the purpose of a software system and the contexts in which it will be used. " +
+                "It refers to the activity of requirement development, which includes elicitation, " +
+                "specification, analysis and management of stakeholder requirements.</p>"
+                + "<br>There are two Requirement Elicitation techniques:<br></br></br>"
+                + "<br><b>Technique 1:</b><br></br></br>"
+                + "<br>&#8226; Examine Documents: reuse existing or related documents, read standards or government regulations.\n</br>"
+                + "<br>&#8226; Interview stakeholders: conduct focus groups with customers to gather idea and needs.\n</br>"
+                + "<br>&#8226; Use questionnaires or marketing surveys for quantitative data.<br></br></br>"
+                + "<br><b>Technique 2:</b><br></br></br>"
+                + "<br>&#8226; Watch users do their job.</br>"
+                + "<br>&#8226; Prototyping: create interface to help customers better understand the system.\n</br>"
+                + "<br>&#8226; Task analysis: make use cases/ scenarios.<br></br></br>"
+                + "<h5> <font color=\"#784C27\"> Requirement Analysis</h5>"
+                + "<p>Decompose high-level requirements into details and identify priorities."
+                + "<br><b>Requirement Speicification Approaches:</b><br></br></br>"
+                + "<br>&#8226; <b>Textual:</b> vision and scope document, use case document, SRS.</br>"
+                + "<br>&#8226; <b>Graphical:</b> structured analysis models (data flow diagram, entity-relationship diagram, dialog map), OO analysis models.</br>"
+                + "<br>&#8226; <b>Formal:</b> formal specification languages.</br>"
+                + "<p><b>SRS</b> is a set of precisely-stated properties and constraints " +
+                "that a software system must satisfy, and a complete description of the external " +
+                "behaviour of a system. The objectives of SRS are to achieve agreement regarding the " +
+                "requirements among developers, customers and other stakeholders, as well as provide basis for design and system testing.</p>";
+
 
         int worldID = intent.getIntExtra("worldID", -1);
         String content = String.valueOf(worldID);
@@ -52,7 +81,7 @@ public class ContentActivity extends AppCompatActivity {
         }
 
         TextView content_tv = findViewById(R.id.contentBlock);
-        content_tv.setText(content);
+        content_tv.setText(Html.fromHtml(planning));
     }
 
     @Override
