8afc167f76da9bc8f48b80d32c7edcdc3d59dd5a
Lawrann Chan <lawrannc@gmail.com>
2019-10-22 00:29:35 +0800

    progressbar

--- app/src/main/java/com/example/pokemonacademy/Control/FinalQuiz.java
+++ app/src/main/java/com/example/pokemonacademy/Control/FinalQuiz.java
@@ -12,5 +12,18 @@ public class FinalQuiz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_quiz);
+
+        // For timer
+//        int timeTaken = 0;
+//        final Timer startTime = new Timer();
+//        final ProgressBar timeProgressBar = (ProgressBar)findViewById(R.id.userpokemonhealth);
+//        TimerTask tt = new TimerTask(){
+//            @Override
+//            public void run (){
+//                timeTaken++;
+//                timeProgressBar.setProgress(timeTaken);
+//            }
+//        };
+//        startTime.schedule(tt,0,1000);
     }
 }
