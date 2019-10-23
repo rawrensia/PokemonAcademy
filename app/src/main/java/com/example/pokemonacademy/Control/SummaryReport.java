1a28b43bf61d345c1b46c6edb525c3160a4369e1
Lawrann Chan <lawrannc@gmail.com>
2019-10-23 08:26:59 +0800

    added maggie's part

---
+++ app/src/main/java/com/example/pokemonacademy/Control/SummaryReport.java
@@ -0,0 +1,53 @@
+package com.example.pokemonacademy.Control;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.pokemonacademy.R;
+
+public class SummaryReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary_report);
+
+        Intent intent = getIntent();
+        final int user_id = intent.getIntExtra("userId", -1);
+        Log.i("HERE","user_id "+user_id);
+        // TODO get user from db and parse details to get user_type
+        // Hardcode user to teacher
+        char user_type = 'T';
+
+        if(user_type == 'T'){
+            CardView indivReport = (CardView)findViewById(R.id.Indiv_Report);
+            CardView allReport = (CardView)findViewById(R.id.All_Report);
+
+            indivReport.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view){
+                    Intent Layer = new Intent(SummaryReport.this, IndividualReportList.class);
+                    startActivity(Layer);
+                }
+            });
+
+            allReport.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view){
+                    Intent Layer = new Intent(SummaryReport.this, OverallSummaryReport.class);
+                    startActivity(Layer);
+                }
+            });
+
+        }
+
+        else{
+            Intent Layer = new Intent(SummaryReport.this, IndividualSummaryReport.class);
+            Layer.putExtra("userId", user_id);
+            startActivity(Layer);
+        }
+    }
+}
